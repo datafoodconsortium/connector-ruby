3fae8b8c19a28954023bcc9d1c84584e7824fce6
Maikel Linke <maikel@email.org.au>
2023-02-01 14:40:54 +1100

    Add test covering the README example
    Your can just call `rake` to execute tests.

---
+++ test/test_connector.rb
@@ -0,0 +1,33 @@
+require "minitest/autorun"
+require 'datafoodconsortium/connector'
+
+class ConnectorTest < Minitest::Test
+  def test_export
+    connector = DataFoodConsortium::Connector::Connector.instance
+
+    connector.loadMeasures(parse_json_file("measures.json"))
+    connector.loadFacets(parse_json_file("facets.json"))
+    connector.loadProductTypes(parse_json_file("productTypes.json"))
+
+    tomato = DataFoodConsortium::Connector::SuppliedProduct.new(
+      "Tomato", "Awesome tomato"
+    )
+    tomato.semanticId = "https://myplatform.com/tomato"
+    tomato.addCertification(connector.FACETS.CERTIFICATION.BIODYNAMICLABEL.DEMETER)
+
+    exported_tomato = connector.export(tomato)
+    assert_equal tomato_json, exported_tomato
+  end
+
+  def parse_json_file(name)
+    JSON.parse(
+      File.read(
+        File.join(File.dirname(__FILE__), name)
+      )
+    )
+  end
+
+  def tomato_json
+    '{"@context":"http://static.datafoodconsortium.org/ontologies/context.json","@type":"dfc-b:SuppliedProduct","dfc-b:name":"Tomato","dfc-b:description":"Awesome tomato","dfc-b:hasCertification":"dfc-f:Demeter","@id":"https://myplatform.com/tomato"}'
+  end
+end
