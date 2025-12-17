0116af4fc39da68e607f77d2df41eb28f4a850a5
Maikel Linke <maikel@email.org.au>
2025-12-17 13:16:47 +1100

    DRY context URI

--- lib/datafoodconsortium/connector/context.rb
+++ lib/datafoodconsortium/connector/context.rb
@@ -9,28 +9,29 @@ module DataFoodConsortium
     # Similar to: https://github.com/ruby-rdf/json-ld-preloaded/
     class Context < JSON::LD::Context
       URL = "https://www.datafoodconsortium.org"
+      URL_NORMALISED = "http://www.datafoodconsortium.org/"
 
       # All context URIs have to use http because https is normalised to http
       # during the lookup of preloaded contexts.
-      add_preloaded("http://www.datafoodconsortium.org/") { parse(json) }
+      add_preloaded(URL_NORMALISED) { parse(json) }
 
       # This is the current file the DFC website refers to in a link header.
       alias_preloaded(
         "http://www.datafoodconsortium.org/wp-content/plugins/wordpress-context-jsonld/context_1.16.0.jsonld",
-        "http://www.datafoodconsortium.org/"
+        URL_NORMALISED
       )
 
       # This was the file the DFC website refers to in a link header.
       alias_preloaded(
         "http://www.datafoodconsortium.org/wp-content/plugins/wordpress-context-jsonld/context.jsonld",
-        "http://www.datafoodconsortium.org/"
+        URL_NORMALISED
       )
 
       # This is the old URL that's not online anymore.
       # Keep it for compatiblity with all versions before 1.8.
       alias_preloaded(
         "http://static.datafoodconsortium.org/ontologies/context.json",
-        "http://www.datafoodconsortium.org/"
+        URL_NORMALISED
       )
 
       # The hash serializer expects only string values in the context.
